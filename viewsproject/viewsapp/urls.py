965ad62cf52d5f8f1f37e881f09972952be88bed
cesarSPC <149351724+cesarSPC@users.noreply.github.com>
2024-07-31 22:59:01 -0500

    New demo endpoints added

--- viewsproject/viewsapp/urls.py
+++ viewsproject/viewsapp/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('demo/', views.index),
+    path('getuser/<str:name>/<int:id>', views.pathview, name='pathview'),
+    path('getuser/', views.qryview, name='qryview'),
+    path("showform/", views.showform, name="showform"),
+    path("getform/", views.getform, name='getform'),
+    path('drinks/<str:drink_name>', views.drinks, name="drink_name"),
 ]
--- viewsproject/viewsapp/views.py
+++ viewsproject/viewsapp/views.py
@@ -14,4 +14,31 @@ def index(request):
 <p>Request path : " {}</p> 
 <p>Request Method :{}</p></center> 
 '''.format(path, method) 
-    return HttpResponse(content) 
+    return HttpResponse(content)
+
+def pathview(request, name, id): 
+    return HttpResponse("Name:{} UserID:{}".format(name, id))
+
+def qryview(request): 
+    name = request.GET['name'] 
+    id = request.GET['id'] 
+    return HttpResponse("Name:{} UserID:{}".format(name, id)) 
+
+def showform(request): 
+    return render(request, "form.html")
+
+def getform(request): 
+    if request.method == "POST": 
+        id=request.POST['id'] 
+        name=request.POST['name'] 
+    return HttpResponse("Name:{} UserID:{}".format(name, id)) 
+
+def drinks(request, drink_name):
+    dic = {
+        "mocha" : "type of coffee",
+        "tea" : "type of beverage",
+        "lemonade" : "type of refreshment"
+    }
+
+    choice_of_drink = dic.get(drink_name)
+    return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
