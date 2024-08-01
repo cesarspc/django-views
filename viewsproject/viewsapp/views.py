36b10a79010a2516ce2d839100e5c1e1816e2d42
cesarSPC <149351724+cesarSPC@users.noreply.github.com>
2024-07-31 21:51:35 -0500

    Create demo and home views

--- viewsproject/viewsapp/views.py
+++ viewsproject/viewsapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Little Lemon Restaurant")
+
+def index(request): 
+    path = request.path 
+    method = request.method 
+    content=''' 
+<center><h2>Testing Django Request Response Objects</h2> 
+<p>Request path : " {}</p> 
+<p>Request Method :{}</p></center> 
+'''.format(path, method) 
+    return HttpResponse(content) 
